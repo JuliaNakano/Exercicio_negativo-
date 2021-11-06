661009d465880395823d92a1dbeb8eec5daefc07
JuliaNakano <jumartinsnakano@gmail.com>
2021-11-05 21:37:10 -0300

    Solução do Exercício negativo

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio_negativo_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //caso o usuario digite um numero menor que 0 apresentar a seguinte mensagem você digitou um numero negativo
+            
+            double numeronegativo;
+            
+            Console.Write("\nDigite um número: ");
+            numeronegativo = Convert.ToDouble(Console.ReadLine());
+
+            if(numeronegativo<0)
+            {
+                Console.WriteLine("O número que você digitou é negativo.");
+            }
+            Console.WriteLine("");
+        }
+    }
+}
